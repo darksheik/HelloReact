967b228424ecd44fac3e60c3b7f22e6c390ac95f
Don Pflaster <dpflaster@gmail.com>
2016-09-06 15:20:50 -0400

    Initial Commit

---
+++ public/app.jsx
@@ -0,0 +1,40 @@
+var Greeter = React.createClass({
+	getDefaultProps: function() {
+		return {
+			name: 'React',
+			message: 'This is the default message!'
+		};
+	},
+	onButtonClick: function(e) {
+		e.preventDefault();
+
+		var name = this.refs.name.value;
+
+		alert(name);
+	},
+	render: function() {
+		var name = this.props.name;
+		var message = this.props.message;
+
+		return (
+			<div>
+			  <h1>Hello {name}!</h1>
+				<p>{message}</p>
+
+				<form onSubmit={this.onButtonClick}>
+				  <input type="text" ref="name"/>
+					<button>Set Name</button>
+				</form>
+
+			</div>
+		);
+	}
+});
+
+var firstName = 'Don';
+var message = 'This is from the component!';
+
+ReactDOM.render(
+  <Greeter name={firstName} message={message}/>,
+	document.getElementById('app')
+);
